5bb36747d886d4c4f227751d24c85d7b3e376e87
Aaron Darwish <aaron7darwish@gmail.com>
2022-01-26 14:48:39 +0000

    Return to the main directory after you recovered the dotfiles.

--- backup.py
+++ backup.py
@@ -10,6 +10,6 @@ if backup_or_recover == 1:
     os.system("cp -r ~/.zshrc ~/.xinitrc ~/.tmux.conf ~/.config/nvim/init.vim ~/.config/zathura/zathurarc ~/.alacritty.yml ./backup")
 elif backup_or_recover == 2:
     print("Recovering...\n")
-    os.system("cd backup && cp -r .zshrc ~/.zshrc; cp -r .xinitrc ~/.xinitrc; cp -r .tmux.conf ~/.tmux.conf; cp -r .init.vim ~/.config/nvim/; cp -r zathurarc .config/zathura/; cp -r .alacritty.yml ~/.alacritty.yml")
+    os.system("cd backup && cp -r .zshrc ~/.zshrc; cp -r .xinitrc ~/.xinitrc; cp -r .tmux.conf ~/.tmux.conf; cp -r .init.vim ~/.config/nvim/; cp -r zathurarc .config/zathura/; cp -r .alacritty.yml ~/.alacritty.yml && cd ../")
 else:
     exit
