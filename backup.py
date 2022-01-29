d37468d430b1ea7b4fccf96b3359099ab2f89d34
Aaron Darwish <60323705+aaron-darwish@users.noreply.github.com>
2022-01-29 00:26:06 +0000

    Update backup.py

--- backup.py
+++ backup.py
@@ -2,8 +2,7 @@
 
 import os
 
-backup_or_recover = input("\nBackup [1] - Recover [2]\n")
-backup_or_recover = int(backup_or_recover)
+backup_or_recover = int(input("\nBackup [1] - Recover [2]\n"))
 
 if backup_or_recover == 1:
     print("Backing up...\n")
