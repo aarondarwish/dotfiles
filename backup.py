3813929028c8a5f5137be3fadc78056c2a9972dc
Aaron D. <aaron.darwish@protonmail.com>
2024-12-17 17:22:32 +0000

    Updated the dotfiles.

--- backup.py
+++ backup.py
@@ -6,9 +6,9 @@ backup_or_recover = int(input("\nBackup [1] - Recover [2]\n"))
 
 if backup_or_recover == 1:
     print("Backing up...\n")
-    os.system("cd backup && cp ~/.zshrc ./ && cp ~/.xinitrc ./ && cp ~/.tmux.conf ./ && cp ~/.config/zathura/zathurarc ./ && cp ~/.config/nvim/init.vim ./ && cd ..")
+    os.system("cd backup && cp ~/.config/picom.conf ./ && cp ~/.zshrc ./ && cp ~/.xinitrc ./ && cp ~/.tmux.conf ./ && cp ~/.config/zathura/zathurarc ./ && cp ~/.config/nvim/init.vim ./ && cd ..")
 elif backup_or_recover == 2:
     print("Recovering...\n")
-    os.system("cd backup && cp -r .zshrc ~/.zshrc; cp -r .xinitrc ~/.xinitrc; cp -r .tmux.conf ~/.tmux.conf; cp -r zathurarc ~/.config/zathura/; cp -r init.vim ~/.config/nvim/ && cd ../")
+    os.system("cd backup && cp -r picom.conf ~/.config/picom.conf; cp -r .zshrc ~/.zshrc; cp -r .xinitrc ~/.xinitrc; cp -r .tmux.conf ~/.tmux.conf; cp -r zathurarc ~/.config/zathura/; cp -r init.vim ~/.config/nvim/ && cd ../")
 else:
     exit
